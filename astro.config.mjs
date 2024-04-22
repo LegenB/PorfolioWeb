00a38a8ce8ff70c9e11b2a0994486c2c2c1f17f1
Byron <58144346+LegenB@users.noreply.github.com>
2024-04-22 16:40:07 -0400

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Porfolio.github.io',
+  site: 'https://LegenB.github.io',
   base: 'Porfolio',
   integrations: [tailwind()]
 
