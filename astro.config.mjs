fcd7ce0792339fe74fc86089cfde90527b41ba30
Byron <58144346+LegenB@users.noreply.github.com>
2024-04-22 16:45:34 -0400

    eliminado github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://LegenB.github.io',
-  base: 'Porfolio',
   integrations: [tailwind()]
 
 });
